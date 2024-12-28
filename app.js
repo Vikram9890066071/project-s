52e542a8990512f5fd2cca4c214d427cd1945111
Vikram9890066071 <vikramwaghmare210@gmail.com>
2024-12-28 18:57:29 +0530

    this is first commit6

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: '52.72.129.133',
+    user: 'root',
+    password: 'Vikram@210',
+    database: 'registration_db'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
+
+// Root URL route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Registration API');
+});
+
+app.post('/register', async (req, res) => {
+    const { first_name, last_name, mobile_number, password, created_by } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const sql = 'CALL CreateUser(?, ?, ?, ?, ?)';
+    db.query(sql, [first_name, last_name, mobile_number, hashedPassword, created_by], (err, result) => {
+        if (err) throw err;
+        res.send('User registered');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
